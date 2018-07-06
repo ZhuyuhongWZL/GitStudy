3fd2bbe647bb87c614388f2536f27b94884f127b
zhuyuhongWZL <673716884@qq.com>
2018-07-06 09:27:08 +0800

    第一次编写的代码

--- app/src/main/java/com/example/win10/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/win10/firstapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.win10.firstapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void doClick(View view){
+        switch (view.getId()){
+            case R.id.Welcome:
+                Toast.makeText(MainActivity.this,"欢迎来到完美世界",Toast.LENGTH_SHORT).show();
+        }
     }
 }
